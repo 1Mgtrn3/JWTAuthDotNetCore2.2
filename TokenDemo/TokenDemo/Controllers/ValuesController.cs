ccd2037ee0b44cf0f66d6d1ab9fd2b33438da5ec
Demid Zhukov <1demidz3@gmail.com>
2019-05-13 00:40:51 +0300

    Initial Commit (v2)

---
+++ TokenDemo/TokenDemo/Controllers/ValuesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TokenDemo.Data;
+
+namespace TokenDemo.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private ApplicationDbContext context;
+
+        public ValuesController(ApplicationDbContext context) {
+            this.context = context;
+
+        }
+
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<List<ApplicationUser>> Get()
+        {
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            
+            return context.Users.ToList(); //$"Current user is {userId}";//
+            //return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
